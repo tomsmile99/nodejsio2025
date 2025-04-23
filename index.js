c435d7c98e73225512f407920d15df53b581dce8
tomsmile99 <babyking2532@gmail.com>
2025-04-23 22:58:33 +0700

    Update 15

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const http = require('http');
-//const https = require('https');
+//const http = require('http');
+const https = require('https');
 const { Server } = require('socket.io');
 
 const fs = require('fs');
@@ -18,11 +18,11 @@ app.use(express.json());
 // const server = http.createServer(app);
 // const wss = new WebSocket.Server({ server });
 
-//const server = http.createServer(app);
-const server = http.createServer({
-  cert: fs.readFileSync('/ssl/cert.pem'),
-  key: fs.readFileSync('/ssl/privkey.pem')
-}, app);
+const server = https.createServer(app);
+// const server = http.createServer({
+//   cert: fs.readFileSync('/ssl/cert.pem'),
+//   key: fs.readFileSync('/ssl/privkey.pem')
+// }, app);
 const io = new Server(server, {
   cors: {
     origin: '*', // หรือ '*'
