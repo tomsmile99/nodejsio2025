c0b0bae1c90e1f092ba945035b2aa748b5a08263
tomsmile99 <babyking2532@gmail.com>
2025-04-23 22:52:38 +0700

    Update 12

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-//const http = require('http');
-const https = require('https');
+const http = require('http');
+//const https = require('https');
 const { Server } = require('socket.io');
 
-const fs = require('fs');
+//const fs = require('fs');
 // const WebSocket = require('ws');
 
 
@@ -18,14 +18,14 @@ app.use(express.json());
 // const server = http.createServer(app);
 // const wss = new WebSocket.Server({ server });
 
-//const server = http.createServer(app);
-const server = https.createServer({
-  cert: fs.readFileSync('cert.pem'),
-  key: fs.readFileSync('privkey.pem')
-}, app);
+const server = http.createServer(app);
+// const server = https.createServer({
+//   cert: fs.readFileSync('cert.pem'),
+//   key: fs.readFileSync('privkey.pem')
+// }, app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://watershop25.tsmiledev.com/', // หรือ '*'
+    origin: '*', // หรือ '*'
     methods: ['GET', 'POST']
   }
 });
